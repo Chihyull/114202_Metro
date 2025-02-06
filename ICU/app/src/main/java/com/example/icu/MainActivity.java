3b1b9fc7125685cded1c7439c982b5fae8e0c582
11336015 <11336015@ntub.edu.tw>
2025-02-06 08:33:02 +0000

    home, setting

--- ICU/app/src/main/java/com/example/icu/MainActivity.java
+++ ICU/app/src/main/java/com/example/icu/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.icu;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,10 +14,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button login_button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login_button = findViewById(R.id.login_button);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
