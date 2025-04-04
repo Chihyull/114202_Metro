35837011e05887db6f11d1fec5c702a18048805e
chihyu <11336010@ntub.edu.tw>
2025-04-05 03:48:29 +0800

    home_int done

--- 114202_metro/app/build.gradle.kts
+++ 114202_metro/app/build.gradle.kts
@@ -37,7 +37,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("mysql:mysql-connector-java:8.0.21")
+    implementation("com.google.android.material:material:1.12.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
 }
