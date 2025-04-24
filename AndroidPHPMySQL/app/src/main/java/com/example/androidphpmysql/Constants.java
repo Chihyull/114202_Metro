3939bdc238115309b247217499f3d6c04461d105
chihyu <11336010@ntub.edu.tw>
2025-04-24 16:38:37 +0800

    範例檔

---
+++ AndroidPHPMySQL/app/src/main/java/com/example/androidphpmysql/Constants.java
@@ -0,0 +1,6 @@
+package com.example.androidphpmysql;
+
+public class Constants {
+    private static final String ROOT_URL = "http://140.131.115.94/Android/v1/";
+    public static final String URL_REGISTER = ROOT_URL + "registerUser.php";
+}
