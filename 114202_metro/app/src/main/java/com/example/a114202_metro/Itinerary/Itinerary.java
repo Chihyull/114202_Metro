076eca3467fc4b474d6922ae1db00b287e357920
chihyu <11336010@ntub.edu.tw>
2025-04-29 22:34:38 +0800

    My Itinerary/Setting interface

--- 114202_metro/app/src/main/java/com/example/a114202_metro/Itinerary/Itinerary.java
+++ 114202_metro/app/src/main/java/com/example/a114202_metro/Itinerary/Itinerary.java
@@ -1,9 +1,10 @@
-package com.example.a114202_metro;
+package com.example.a114202_metro.Itinerary;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,37 +12,30 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.a114202_metro.Itinerary.Itinerary;
+import com.example.a114202_metro.R;
 
-public class MainActivity extends AppCompatActivity {
+public class Itinerary extends AppCompatActivity {
 
-    ImageView btn_login;
+    Button btn_create;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_itinerary);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        btn_create = findViewById(R.id.btn_create);
 
-        btn_login = findViewById(R.id.btn_login);
-
-        btn_login.setOnClickListener(new View.OnClickListener() {
+        btn_create.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Itinerary.class);
+                Intent intent = new Intent(Itinerary.this, ItinerarySetting.class);
                 startActivity(intent);
             }
         });
-
-
-
-
     }
-
-
 }
--- 114202_metro/app/src/main/java/com/example/a114202_metro/Itinerary/ItinerarySetting.java
+++ 114202_metro/app/src/main/java/com/example/a114202_metro/Itinerary/ItinerarySetting.java
@@ -1,9 +1,6 @@
-package com.example.a114202_metro;
+package com.example.a114202_metro.Itinerary;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,37 +8,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.a114202_metro.Itinerary.Itinerary;
+import com.example.a114202_metro.R;
 
-public class MainActivity extends AppCompatActivity {
-
-    ImageView btn_login;
+public class ItinerarySetting extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_itinerary_setting);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        btn_login = findViewById(R.id.btn_login);
-
-        btn_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Itinerary.class);
-                startActivity(intent);
-            }
-        });
-
-
-
-
     }
-
-
 }
