963d8af84ca673089a6ee88475f6b1a39fe2928c
chihyu <11336010@ntub.edu.tw>
2025-10-04 12:02:46 +0800

    更新

--- 114202_metro/app/src/main/java/com/example/a114202_metro/Station/ExitModel.java
+++
@@ -1,8 +0,0 @@
-package com.example.a114202_metro.Station;
-
-public class ExitModel {
-    private final String exitCode;
-
-    public ExitModel(String exitCode) { this.exitCode = exitCode; }
-    public String getExitCode() { return exitCode; }
-}
